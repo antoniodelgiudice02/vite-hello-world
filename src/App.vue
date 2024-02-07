ffd339413aacf1be4ea83a73d602d342a2b2e25d
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-02-07 17:13:45 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+
+import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue'
+
+export default {
+  data() {
+  },
+
+  components: { 
+    AppTitle,
+    AppHero
+   }
+}
+
+</script>
+
+<template>
+
+  <div>
+    <AppTitle/>
+    <AppHero/>
+  </div>
+  
+</template>
+
+<style></style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+    return {
+      img: 'https://positivethinking.tech/wp-content/uploads/2021/01/Logo-Vuejs.png'
+    }
+  },
+}
+</script>
+
+<template>
+
+    <div>
+        <img :src="img" alt="">
+    </div>
+
+</template>
+
+<style>
+
+</style>
